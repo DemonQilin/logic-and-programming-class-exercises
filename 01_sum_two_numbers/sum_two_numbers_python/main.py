7075de8686b56b8ca3a160ff5d143eebe4c910c1
DemonQilin <velezloaizaesteban9012015@gmail.com>
2024-02-16 13:48:20 -0500

    Add python version of sum of two numbers

---
+++ 01_sum_two_numbers/sum_two_numbers_python/main.py
@@ -0,0 +1,13 @@
+def main():
+    print("--PROGRAMA PARA LA SUMA DE DOS NUMEROS--\n")
+
+    first_number = int(input("Ingrese el valor del primer número: "))
+    second_number = int(input("Ingrese el valor del segundo número: "))
+
+    total = first_number + second_number
+
+    print(f"\nLa suma de {first_number} y {second_number} es: {total}")
+
+
+if __name__ == '__main__':
+    main()
