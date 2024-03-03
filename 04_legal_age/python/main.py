7584cef65f65be25c156a0ef5d8e6ba6ad46aab2
DemonQilin <velezloaizaesteban9012015@gmail.com>
2024-03-03 12:32:40 -0500

    Add python code for exercise 4: determinar mayoria de edad

---
+++ 04_legal_age/python/main.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    print("\n--- PROGRAMA PARA DETERMINAR MAYORIA DE EDAD ---\n")
+
+    age = int(input("Ingrese una edad > "))
+
+    if age >= 18:
+        print("\nLa persona es MAYOR de edad")
+    else:
+        print("\nLa persona es MENOR de edad")
